ae108985683fc76e1fb150908caf0e11d394b4b4
filmscore88 <cmtmorales24@gmail.com>
2019-07-09 18:36:23 -0500

    added javascript class constructor for json data

--- app/assets/javascripts/invention.js
+++ app/assets/javascripts/invention.js
@@ -1,4 +1,37 @@
 
 $(function() {
-    console.log("Test")
+    getInventions();
 });
+
+
+// JS model object to use for JSON data manipulation
+class Invention {
+  constructor(id, name, description, user_id) {
+    this.id = id;
+    this.name= name;
+    this.description= description;
+    this.user_id= user_id;
+  }
+}
+
+
+// Method for model object prototype to append HTML to DOM
+Invention.prototype.addHTML= function(){
+  return(
+    `<ul>
+      <li>Name: ${this.name}</li>
+      <li>Inventor: ${this.user.name}</li>
+    </ul>`
+  )
+}
+
+function getInventions(){
+  $.ajax({
+    url: "http://localhost:3000/inventions",
+    method: "get",
+    dataType: "json"
+
+  }).done(function (data){
+    console.log(data)
+  })
+}
