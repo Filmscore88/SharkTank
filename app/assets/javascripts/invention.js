13cd3322898744c48c21c91bc93a129fb461d971
filmscore88 <cmtmorales24@gmail.com>
2019-07-14 23:40:40 -0500

    added seperate functions for ajax calls

--- app/assets/javascripts/invention.js
+++ app/assets/javascripts/invention.js
@@ -1,6 +1,7 @@
 
 $(function() {
-    activateListeners();
+  activateInventions();
+  submit();
 });
 
 
@@ -30,7 +31,6 @@ Invention.prototype.showHTML= function(){
     <h3>Inventor: ${this.user_name}</h3>
     <h3>Description: ${this.description}</h3>
     <h3>Invention Investments total: $${this.invention_investments.sum("amount")}<h3>`
-
   )
 }
 
@@ -44,49 +44,31 @@ Array.prototype.sum = function (prop) {
 }
 
 
-// Listener functions to be run on document ready
-function activateListeners(){
-  activateInventions();
-  invSubmit();
-}
 
 // Listeners
 function activateInventions(){
-$('#get-inventions').on('click', function(e) {
-  $('#html_format').html('')
-   history.replaceState(null, null, "/inventions");
-   e.preventDefault();
-   getInventions();
- });
+
+  $('#get-inventions').on('click', function(e) {
+     e.preventDefault();
+     $('#html_format').html('')
+     history.replaceState(null, null, "/inventions");
+     getInventions();
+  });
 
   $(document).on('click', ".show_link", function(e){
     e.preventDefault();
     $('#invention_inserts').html('')
     let id= $(this).attr('data-id')
-
-    fetch(`inventions/${id}.json`)
-    .then(res => res.json())
-    .then(invention => {
-      let newInvention= new Invention(invention)
-      let inventionHTML= newInvention.showHTML()
-      $('#invention_inserts').append(inventionHTML)
-    })
+    getInvention(id)
   })
 }
 
 
-  function invSubmit(){
+  function submit(){
     $("#newInv").on("submit", function(e){
       e.preventDefault();
-
       const values= $(this).serialize()
-
-      $.post("/inventions", values).done(function(data){
-        $("#app-container").html("");
-        const newInv= new Invention(data)
-        const html= newInv.showHTML()
-        $("#app-container").append(html)
-      })
+      submitInvention(values)
     })
   }
 
@@ -105,4 +87,25 @@ function getInventions(){
     });
       $('#title-placement').append("<h1> Inventions</h1>")
   });
+}
+
+
+function getInvention(id){
+  fetch(`inventions/${id}.json`)
+  .then(res => res.json())
+  .then(invention => {
+    let newInvention= new Invention(invention)
+    let inventionHTML= newInvention.showHTML()
+    $('#invention_inserts').append(inventionHTML)
+  })
+}
+
+
+function submitInvention(values){
+  $.post("/inventions", values).done(function(data){
+    $("#app-container").html("");
+    const newInv= new Invention(data)
+    const html= newInv.showHTML()
+    $("#app-container").append(html)
+  })
 }
